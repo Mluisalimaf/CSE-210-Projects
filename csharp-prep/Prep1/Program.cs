be9f23f2b4c2ffaecafb58160c96525d4c8fbf59
maria luisa <limamarialuisa06@gmail.com>
2024-09-19 17:51:55 -0300

    Added a comment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {  
+        //Ask a user for their name and display it like the famous James Bond phrase
         Console.WriteLine("What is your fist name?");
         string first = Console.ReadLine();
 
