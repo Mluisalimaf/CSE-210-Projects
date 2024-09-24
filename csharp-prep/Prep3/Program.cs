580d30e0f7dd1ad3c6d0b3dcd8dc532cfac25984
maria luisa <limamarialuisa06@gmail.com>
2024-09-24 00:02:11 -0300

    assignment finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -9,12 +9,13 @@ class Program
 
         Random randomGenerator = new Random();
         int number = randomGenerator.Next(1, 101);
+        
         int guess = -1;
 
 
         while (guess != number)
         {
-            Console.WriteLine("What is your guess? ");
+            Console.Write("What is your guess? ");
             guess = int.Parse(Console.ReadLine());
 
             if (number > guess)
@@ -31,8 +32,6 @@ class Program
             }
 
         }
-
-
 
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        //put the variables you need
+        int enterNumber;
+        int sum = 0;
+        int count = 0; 
+        int maxValue = int.MinValue;
+
+        //now do the loops
+        do
+        {
+            Console.Write("Enter number: ");
+            enterNumber = int.Parse(Console.ReadLine());
+
+            if (enterNumber != 0)
+            {
+                sum += enterNumber;
+                count++;
+
+                if (enterNumber > maxValue)
+                {
+                    maxValue = enterNumber;
+                } 
+            }    
+        }while(enterNumber != 0);
+
+        //You use double when the number can be a decimal value, rather than just an integer
+        double average = count > 0 ? (double)sum / count: 0;
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average number is: {average}");
+        Console.WriteLine($"The largest number is: {maxValue}");
     }
 }
