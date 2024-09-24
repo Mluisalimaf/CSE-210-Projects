d91c84cef7b1a7e996063bf4eec05f91acc432ff
maria luisa <limamarialuisa06@gmail.com>
2024-09-24 01:42:31 -0300

    finished!

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,7 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
+        //there are two ways of creating this code:
+        //the first one using lists:
+
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int>();
+        
+        int enterNumber = -1;
+        while (enterNumber != 0)
+        {
+            Console.Write("Enter a number: ");
+            
+            string answer = Console.ReadLine();
+            enterNumber = int.Parse(answer);
+            
+            if (enterNumber != 0)
+            {
+                numbers.Add(enterNumber);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        double average = ((double)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int maxValue = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > maxValue)
+            {
+                maxValue = number;
+            }
+        }
+
+        Console.WriteLine($"The largest number is: {maxValue}");
+
+        //the second one using just do-while loops:
+
+        /*Console.WriteLine("Enter a list of numbers, type 0 when finished.");
 
         //put the variables you need
         int enterNumber;
@@ -32,14 +77,13 @@ class Program
 
         /*You use double when the number can be a decimal value, 
         rather than just an integer.
-        
         The ternary operator (? :) is used here: if count > 0, 
         the average is the sum divided by the number of entries; 
-        otherwise, the average is 0*/
+        otherwise, the average is 0*
         double average = count > 0 ? (double)sum / count: 0;
 
         Console.WriteLine($"The sum is: {sum}");
         Console.WriteLine($"The average number is: {average}");
-        Console.WriteLine($"The largest number is: {maxValue}");
+        Console.WriteLine($"The largest number is: {maxValue}");*/
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
     }
+    
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+
+            string name = PromptUserName();
+            int askingNumber = PromptUserNumber();
+            int squaredNumber = SquareNumber(askingNumber);
+
+            DisplayResult(name, squaredNumber);
+        }
+
+        //PromptUserName - Asks for and returns the user's name (as a string)
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            return Console.ReadLine();
+        }
+
+        //PromptUserNumber - Asks for and returns the user's favorite number (as an integer)
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+        
+        //SquareNumber - Accepts an integer as a parameter and returns that number squared (as an integer)
+        static int SquareNumber(int number)
+        {
+            return number * number;
+        }
+        
+        //DisplayResult - Accepts the user's name and the squared number and displays them.
+        static void DisplayResult(string name, int squaredNumber)
+        {
+            Console.WriteLine($"{name}, the square of you number is {squaredNumber}");
+        }
+        
 }
