524df25cfac8d33f01d01fd7ed15d3b6edabe74a
maria luisa <limamarialuisa06@gmail.com>
2024-10-18 00:55:37 -0300

    finished

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment a1 = new Assignment("Samuel Bennet", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment mathassig1 = new MathAssignment("Leonardo Perez", "Division", "7.3", "8-19");
+        Console.WriteLine(mathassig1.GetSummary()); 
+        Console.WriteLine(mathassig1.GetHomeworkList());
+
+        WritingAssignment writ1 = new WritingAssignment(
+            "Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writ1.GetSummary());
+        Console.WriteLine(writ1.GetWritingInformation());
+
     }
 }
